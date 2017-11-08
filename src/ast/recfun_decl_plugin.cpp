51a021c5c3b42dad4ea0de379ead2ce24dc0d81b
Simon Cruanes <simon.cruanes.2007@m4x.org>
2017-11-08 17:11:57 +0100

    wip: definition of (recursive) functions

--- src/ast/recfun_decl_plugin.cpp
+++ src/ast/recfun_decl_plugin.cpp
@@ -15,16 +15,71 @@ Revision History:
 
 --*/
 
+#include "ast/expr_functors.h"
 #include "ast/recfun_decl_plugin.h"
 
 namespace recfun {
-    
-    def::def(symbol s, ast_manager &m, sort* args, unsigned n_args, sort* ret):
-        m_name(s), m_arg_sorts(m), m_ret_sort(ret, m), m_cases() {
-        for (auto i=0; i<n_args; ++i) {
-            sort_ref s(&args[i], m);
-            m_arg_sorts.push_back(s);
+
+    void case_pred::set_name(std::string & s) {
+        m_name_buf = std::string(s); // owned copy
+        m_name = symbol(m_name_buf.c_str());
+    }
+
+    case_def::case_def(ast_manager &m,
+                       std::string & name,
+                       sort_ref_vector const & arg_sorts,
+                       expr* guards, unsigned num_guards, expr* rhs)
+    : m_pred(arg_sorts), m_guards(), m_rhs(expr_ref(rhs,m)) {
+        m_pred.set_name(name);
+        for (unsigned i=0; i<num_guards; ++i) {
+            m_guards.push_back(expr_ref(&guards[i], m));
         }
     }
 
+    def::def(ast_manager &m, symbol s, sort* args, unsigned n_args, sort* ret)
+        :   m_kind(OP_FUN_DEFINED), m_manager(m), m_name(s),
+            m_arg_sorts(m), m_ret_sort(ret, m), m_vars(), m_cases()
+    {
+        for (unsigned i=0; i<n_args; ++i) m_arg_sorts.push_back(sort_ref(&args[i], m));
+    }
+
+    // does `e` contain any `ite` construct?
+    bool def::contains_ite(expr * e) {
+        struct ite_find_p : public i_expr_pred {
+            ast_manager & m;
+            ite_find_p(ast_manager & m) : m(m) {}
+            virtual bool operator()(expr * e) { return m.is_ite(e); }
+        };
+        ite_find_p p(m());
+        check_pred cp(p, m());
+        return cp(e);
+    }
+
+    // Compute a set of cases, given the RHS
+    void def::compute_cases(var * vars, unsigned n_vars, expr* rhs) {
+        std::string name;
+        name.append("case_");
+        name.append(m_name.bare_str());
+
+        for (unsigned i=0; i<n_vars; ++i) m_vars.push_back(&vars[i]);
+
+        if (n_vars == 0 || !contains_ite(rhs)) {
+            // constant function or trivial control flow, only one (dummy) case
+            m_kind = OP_FUN_MACRO;
+            name.append("_dummy");  
+            case_def c0(m(), name, sort_args(), nullptr, 0, rhs);
+            m_cases.push_back(c0);
+            return;
+        }
+
+        // TODO: analyze control flow of `rhs`, building a vector of pair<guard,rhs>,
+        // accumulating guards and rebuilding a `ite`-free RHS on the fly.
+        // Each pair is then converted into a case_def
+    }
+
+    util::util(ast_manager & m) : m_manager(m) {}
+
+    void util::add_fun(symbol s, sort * args, unsigned n_args, var * vars, unsigned n_vars, expr * rhs) {
+        // TODO:
+    }
 }
--- src/ast/recfun_decl_plugin.h
+++ src/ast/recfun_decl_plugin.h
@@ -26,8 +26,9 @@ namespace recfun {
     class util; //<! util for other modules
 
     enum op_kind {
-        OP_FUN_DEFINED,
-        OP_FUN_CASE_PRED,
+        OP_FUN_MACRO, // unfold eagerly, only one case
+        OP_FUN_DEFINED, // defined function with multiple cases, possibly recursive
+        OP_FUN_CASE_PRED, // predicate guarding a given control flow path
     };
 
     /*! A predicate `p(t1…tn)`, that, if true, means `f(t1…tn)` is following
@@ -39,45 +40,68 @@ namespace recfun {
     */
     class case_pred {
         friend class case_def;
-        symbol              m_name; // name of the predicate
-        sort_ref_vector     m_arg_sorts;
+        symbol                      m_name; // symbol for the predicate
+        std::string                 m_name_buf; // memory for m_name
+        sort_ref_vector const &     m_arg_sorts;
 
-        case_pred(symbol && s, sort_ref_vector &&args) : m_name(s), m_arg_sorts(args) {}
+        case_pred(sort_ref_vector const & args) : m_name(), m_name_buf(), m_arg_sorts(args) {}
+        void set_name(std::string & s);
     public:
         symbol const & name() const { return m_name; }
         sort_ref_vector const & args() const { return m_arg_sorts; }
     };
 
     class case_def {
-        friend class decl;
+        friend class def;
         case_pred           m_pred; //<! predicate used for this case
-        ptr_vector<expr>    m_guards; //<! conjunction that is equivalent to this case
+        vector<expr_ref>    m_guards; //<! conjunction that is equivalent to this case
         expr_ref            m_rhs; //<! if guard is true, `f(t1…tn) = rhs` holds
 
-        case_def(ast_manager &m, expr* guards, unsigned num_guards, expr* rhs);
+        case_def(ast_manager & m,
+                 std::string & name,
+                 sort_ref_vector const & arg_sorts,
+                 expr* guards,
+                 unsigned num_guards,
+                 expr* rhs);
+
+        void add_guard(expr_ref && e) { m_guards.push_back(e); }
     public:
         case_pred const & pred() const { return m_pred; }
-        expr * guards() const { return *m_guards.c_ptr(); }
+        vector<expr_ref> const & guards() const { return m_guards; }
+        expr * guards_c_ptr() const { return *m_guards.c_ptr(); }
         expr * guard(unsigned i) const { return m_guards[i]; }
+        expr * rhs() const { return m_rhs; }
         unsigned num_guards() const { return m_guards.size(); }
     };
 
     class def {
         friend class util;
+        typedef ptr_vector<var>  vars;
+        typedef vector<case_def> cases;
+        op_kind             m_kind; //<! kind of function
+        ast_manager &       m_manager;
         symbol              m_name; //<! name of function
-        sort_ref_vector     m_arg_sorts;
+        sort_ref_vector     m_arg_sorts; //<! type of arguments
         sort_ref            m_ret_sort; //<! return type
-        vector<case_def>    m_cases; //!< possible cases
+        vars                m_vars; //<! variables of the function
+        cases               m_cases; //!< possible cases
 
-        def(symbol s, ast_manager &m, sort* args, unsigned n_args, sort* ret);
-    public:
-        vector<case_def> const & cases() const { return m_cases; }
+        ast_manager & m() const { return m_manager; }
+        symbol const & name() const { return m_name; }
+        sort_ref_vector const & sort_args() const { return m_arg_sorts; }
+        sort_ref const & sort_ret() const { return m_ret_sort; }
+        def(ast_manager &m, symbol s, sort* args, unsigned n_args, sort* ret);
 
         // compute cases for a function, given its RHS (possibly containing `ite`).
-        void compute_cases(expr* rhs);
+        void compute_cases(var * vars, unsigned n_vars, expr* rhs);
+
+        bool contains_ite(expr* e); // expression contains a test?
+    public:
+        vars const & get_vars() const { return m_vars; }
+        cases const & get_cases() const { return m_cases; }
     };
 
-    // TODO: decl_plugin
+    // TODO: decl_plugin?
     
     // Varus utils for recursive functions
     class util {
@@ -91,6 +115,9 @@ namespace recfun {
 
         bool is_case_pred(app * e) const { return is_app_of(e, m_family_id, OP_FUN_CASE_PRED); }
         bool is_defined(app * e) const { return is_app_of(e, m_family_id, OP_FUN_DEFINED); }
+
+        // Add a function declaration (TODO: allow mutual definitions)
+        void add_fun(symbol s, sort * args, unsigned n_args, var * vars, unsigned n_vars, expr * rhs);
     };
 
 }
